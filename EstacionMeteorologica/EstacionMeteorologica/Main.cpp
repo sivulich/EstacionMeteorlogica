aca9ed9050a9567c628cf4a7a898b1b24e8c0943
Santi <Santi@LAPTOP-U5VJ59DD>
2017-02-12 17:45:37 -0300

    Diagramado el main, falta: todo.

---
+++ EstacionMeteorologica/EstacionMeteorologica/Main.cpp
@@ -0,0 +1,49 @@
+#pragma once
+
+
+int main(void)
+{
+	/*
+	bool exit=false;
+	network.init();
+	sensors.init();
+	monitor.init();
+	while(exit ==false)
+	{
+		whlie(network.hayEvento()==false);
+		event in=network.getEvent();
+		switch (in.type())
+		{
+			case PING:
+				network.sendSerial();
+				break;
+			case CONNECT_TO_SLAVE:
+				network.sendConnectAck();
+				break;
+			case GET_STATUS:
+				network.sendStatus(monitor.getStatus());
+				break;
+			case DATA_REQUEST:
+				network.sendData(sensors.getData());
+				break;
+			case RESET_SLAVE:
+				monitor.reset();
+				exit=true;
+				break;
+			case EMERGENCY_SHITDOWN:
+				exit(1);
+			case SOFTWARE_UPDATE:
+				network.getUpdate();
+				monitor.update();
+				exit=true;
+				break;
+			case REQUEST_SENSOR_LIST:
+				network.sendSensorList(sensors.getList());
+				break;
+
+		}
+	}
+
+	*/
+	return 0;
+}
